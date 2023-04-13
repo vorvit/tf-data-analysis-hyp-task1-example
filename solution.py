386969f74149c8cc3ad73e1833f1d2d1fbcaf809
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-13 20:35:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,5 +9,5 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    z_score, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    z_score, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='smaller')
     return p_value < 0.07
